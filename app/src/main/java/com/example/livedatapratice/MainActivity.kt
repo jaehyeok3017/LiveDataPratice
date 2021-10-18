3d13fab785935e3a2ebe86d312a284ad0fed0531
samgashyeong <samgashyeong@naver.com>
2021-10-18 18:20:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/livedatapratice/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.livedatapratice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.livedatapratice.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private val firstFragment by lazy { FirstFragment() }
+    private val secondFragment by lazy { SecondFragment() }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        findViewById<BottomNavigationView>(R.id.bottomNav).setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.firstfm -> {
+                    changeFragment(firstFragment)
+                    true
+                }
+                R.id.secondfm ->{
+                    changeFragment(secondFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fm, fragment).commit()
+
+    }
+}
